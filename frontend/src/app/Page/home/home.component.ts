d4226b74df0e4f40b82f7768bda0069d756d4457
RoyalR4z <viniciusviana2004@hotmail.com>
2023-10-04 17:02:32 -0300

    Adding the Page Title

--- frontend/src/app/Page/home/home.component.ts
+++ frontend/src/app/Page/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private metaService: Meta, private titleService: Title) { }
+
+  ngOnInit(): void {
+    // Define o título da página
+    this.titleService.setTitle('NODOT');
+
+    // Define as meta tags
+    this.metaService.addTag({ name: 'description', content: 'Descrição da página' });
+    this.metaService.addTag({ name: 'keywords', content: 'automação, NODOT' });
+  }
 }
